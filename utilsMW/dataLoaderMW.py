5a70c3e94e3e69dd7538076a5031a5277b83da91
Hendrik Elvers <h.elvers@synergeticon.com>
2022-06-27 14:20:49 +0200

    before meta change

--- utilsMW/dataLoaderMW.py
+++ utilsMW/dataLoaderMW.py
@@ -36,10 +36,10 @@ class TorchDatasetMW(torch.utils.data.Dataset):
 
 class TorchDatasetMWToy(torch.utils.data.Dataset):
   def __init__(self, path, device = 'cpu'):
-      path_data = path + 'inpt'
-      path_label = path + 'label'
-      self.data = torch.load(path_data).to(torch.float32).to(device)
-      self.label = torch.load(path_label).to(torch.float32).to(device)
+    path_data = path + 'inpt'
+    path_label = path + 'label'
+    self.data = torch.load(path_data).to(torch.float32).to(device)
+    self.label = torch.load(path_label).to(torch.float32).to(device)
       
 
   def __len__(self):
@@ -51,8 +51,9 @@ class TorchDatasetMWToy(torch.utils.data.Dataset):
         return self.data[index], self.label[index]
 
 class TorchDatasetTailor(torch.utils.data.Dataset):
-    def __init__(self, trajectories, obsv, success, ftrj) -> None:
+    def __init__(self, trajectories, obsv, success, ftrj=None) -> None:
         super().__init__()
+
         self.s_trajectories = trajectories[success==1]
         self.s_ftrj = ftrj[success==1]
         self.s_obsv = obsv[success==1]
@@ -65,14 +66,35 @@ class TorchDatasetTailor(torch.utils.data.Dataset):
 
         self.s_len = len(self.success)
         self.f_len = len(self.fail)
-        self.len = max(self.s_len, self.f_len)
+
+        #self.len = max(self.s_len, self.f_len)
+
+    def add_data(self, trajectories, obsv, success, ftrj):
+        if ((success==1).sum()) > 0:
+            self.s_trajectories = torch.cat((self.s_trajectories, trajectories[success==1]), dim=0)
+            self.s_ftrj = torch.cat((self.s_ftrj, ftrj[success==1]), dim=0)
+            self.s_obsv = torch.cat((self.s_obsv, obsv[success==1]), dim=0)
+            self.success = torch.cat((self.success, success[success==1]), dim=0)
+        
+        if ((success==0).sum()) > 0:
+            self.f_trajectories = torch.cat((self.f_trajectories, trajectories[success==0]), dim=0)
+            self.f_ftrj = torch.cat((self.f_ftrj, ftrj[success==0]), dim=0)
+            self.f_obsv = torch.cat((self.f_obsv, obsv[success==0]), dim=0)
+            self.fail = torch.cat((self.fail, success[success==0]), dim=0)
+
+        self.s_len = len(self.success)
+        self.f_len = len(self.fail)
+
 
     def __len__(self):
-        return self.len
+        return max(self.s_len, self.f_len)
+
+    def num_ele(self):
+        return self.s_len + self.f_len
 
     def __getitem__(self, index):
         return (self.s_trajectories[index%self.s_len], self.s_obsv[index%self.s_len], self.success[index%self.s_len], self.s_ftrj[index%self.s_len]),\
-             (self.f_trajectories[index%self.f_len], self.f_obsv[index%self.f_len], self.fail[index%self.f_len], self.f_ftrj[index%self.f_len])
+            (self.f_trajectories[index%self.f_len], self.f_obsv[index%self.f_len], self.fail[index%self.f_len], self.f_ftrj[index%self.f_len])
 
 if __name__ == '__main__':
     ptd = '/home/hendrik/Documents/master_project/LokalData/metaworld/pick-place/training_data/'
