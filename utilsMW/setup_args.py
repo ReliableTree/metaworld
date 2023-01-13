832e18821a3353ab9b49ed649849dc71db20471a
Hendrik Elvers <h.elvers@synergeticon.com>
2023-01-13 13:37:51 +0100

    last

---
+++ utilsMW/setup_args.py
@@ -0,0 +1,115 @@
+from LanguagePolicies.utils.Transformer import TransformerModel, TailorTransformer, NNSetup
+import torch
+from typing import Optional
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+
+class CriticSetup:
+    def __init__(self) -> None:
+        self.nn = NNSetup()
+        self.nn.use_layernorm = False
+        self.nn.upconv = False
+        self.nn.num_upconvs = 4
+        self.nn.stride = 3
+        self.nn.d_output = 5
+        self.nn.d_model = 60
+        self.nn.nhead = 6
+        self.nn.d_hid = 60
+        self.nn.nlayers = 4
+        self.nn.d_result = 1
+        self.nn.seq_len = 100
+
+        self.optimizer_class = torch.optim.Adam
+        self.model_class: TailorTransformer
+        self.lr = None
+        self.optimizer_kwargs = {}
+
+class ActorSetup:
+    def __init__(self) -> None:
+        self.nn = NNSetup
+
+        self.optimizer_class = torch.optim.AdamW
+        self.model_class: TransformerModel
+        self.lr = None
+        self.optimizer_kwargs = {}
+
+class NetworkSetup:
+    def __init__(self) -> None:
+        self.actor_setup = ActorSetup
+        self.critic_setup = CriticSetup
+
+
+class ActiveCriticArgs:
+    def __init__(self) -> None:
+        pass
+
+    def set_critic_optimisation_threshold(self, optimisation_threshold: float):
+        self.optimisation_threshold = optimisation_threshold
+
+    def set_quick_eval_epochs(self, quick_eval_epochs: int):
+        self.quick_eval_epochs = quick_eval_epochs
+
+    def set_network_setup(self, network_setup: NetworkSetup):
+        self.network_setup = network_setup
+
+    def set_epoch_len(self, epoch_len: int):
+        self.network_setup.critic_nn.seq_len = epoch_len
+        self.network_setup.actor_nn.seq_len = epoch_len
+        self.epoch_len = epoch_len
+
+    def set_new_epoch(self, new_epoch):
+        self.new_epoch = new_epoch
+
+    def set_feature_extractor(self, extractor: BaseFeaturesExtractor):
+        self.extractor = extractor
+
+    def set_data_path(self, path: str, model_path: Optional[str] = None):
+        self.data_path = path
+        if model_path is not None:
+            self.model_path = path + model_path
+        else:
+            self.model_path = None
+
+    def set_log_name(self, logname: str):
+        self.logname = logname
+
+    def set_mlr(self, mlr: float):
+        self.mlr = mlr
+
+    def set_meta_optimizer_lr(self, mo_lr: float):
+        self.network_setup.critic_setup.lr = mo_lr
+
+    def set_lr(self, lr: float):
+        self.network_setup.actor_setup.lr = lr
+
+    def set_demonstrations(self, demonstrations: list):
+        self.demonstrations = demonstrations
+
+    def set_device(self, device: str):
+        self.device = device
+
+    def set_tboard(self, tboard: bool):
+        self.tboard = tboard
+
+    def set_batchsize(self, batch_size: int):
+        self.batch_size = batch_size
+
+    def set_n_steps(self, n_steps: int):
+        self.n_steps = n_steps
+
+    def set_imitation_phase(self, imitation_phase: bool):
+        self.imitation_phase = imitation_phase
+
+    def set_weight_decay(self, weight_decay: float):
+        self.weight_decay = weight_decay
+
+    def set_eval_epochs(self, epochs: int):
+        self.eval_epochs = epochs
+
+    def set_opt_steps(self, opt_steps: int):
+        self.opt_steps = opt_steps
+
+    def set_complete_modulo(self, complete: int):
+        self.complete_modulo = complete
+
+    def set_observable(self, observable: bool):
+        self.observable = observable
